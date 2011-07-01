32880469e96bfe6b8a7d39aacb868d05bb6d6be9
Andrei Bad <andrei.badulescu@gmail.com>
2011-07-01 11:40:12 -0700

    added more code from Camel book

--- Test.pl
+++ Test.pl
@@ -6,7 +6,13 @@ use Tools;
 use Data::Dumper;
 my @array = qw(13 4 7 2 9 8 5);
 
-my @sorted = numerically_sort(@array);
 
-print Dumper(@sorted);
+my ($a1, $a2, $a3, $a4, $a5, $a6, $a7) = @array;
+
+print Dumper ($a1, $a7);
+
+
+#my @sorted = numerically_sort(@array);
+
+#print Dumper(@sorted);
 
