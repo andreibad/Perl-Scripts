d6202d7621a146b875efb3aadbe047dff04aafe1
Andrei Bad <andrei.badulescu@gmail.com>
2011-08-08 17:23:38 -0700

    added more sax pasing

--- sax.pl
+++ sax.pl
@@ -1,6 +1,10 @@
-use warnings;
 use strict;
+use warnings;  
 use XML::SAX;
+use MySAXHandler;
 
+my $parser = XML::SAX::ParserFactory->parser(
+		Handler => MySAXHandler->new
+		);
 
-
+$parser->parse_uri($ARGV[0]);
