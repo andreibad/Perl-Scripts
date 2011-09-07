dda74274a0d6942620161a1edc6eda9adbbc7690
Andrei Bad <andrei.badulescu@gmail.com>
2011-09-07 09:30:18 -0700

    ran perltidy

--- LinkedList/LinkedList.pl
+++ LinkedList/LinkedList.pl
@@ -1,76 +1,71 @@
-use strict ;
+use strict;
 use warnings;
 
 use Data::Dumper;
 use LinkedListNode;
 
 my $head = LinkedListNode->new(0);
-$head->setSuccessor (new LinkedListNode(1));
+$head->setSuccessor( new LinkedListNode(1) );
 my $n1 = $head->getSuccessor();
-$n1->setSuccessor(new LinkedListNode(2));
+$n1->setSuccessor( new LinkedListNode(2) );
 my $n2 = $n1->getSuccessor();
-$n2->setSuccessor(new LinkedListNode(3));
+$n2->setSuccessor( new LinkedListNode(3) );
 my $n3 = $n2->getSuccessor();
-$n3->setSuccessor(new LinkedListNode(4));
+$n3->setSuccessor( new LinkedListNode(4) );
 my $n4 = $n3->getSuccessor();
-$n4->setSuccessor(new LinkedListNode(5));
+$n4->setSuccessor( new LinkedListNode(5) );
 my $n5 = $n4->getSuccessor();
-$n5->setSuccessor(new LinkedListNode(6));
+$n5->setSuccessor( new LinkedListNode(6) );
 my $n6 = $n5->getSuccessor();
-$n6->setSuccessor(new LinkedListNode(7));
+$n6->setSuccessor( new LinkedListNode(7) );
 my $n7 = $n6->getSuccessor();
-$n7->setSuccessor(new LinkedListNode(8));
-my $n8= $n7->getSuccessor();
-$n8->setSuccessor(new LinkedListNode(9));
+$n7->setSuccessor( new LinkedListNode(8) );
+my $n8 = $n7->getSuccessor();
+$n8->setSuccessor( new LinkedListNode(9) );
 my $n9 = $n8->getSuccessor();
-$n9->setSuccessor(new LinkedListNode(10));
+$n9->setSuccessor( new LinkedListNode(10) );
 my $n10 = $n9->getSuccessor();
 $n10->setSuccessor($head);
 
-
-
-
-
-
-#print Dumper($head); 
+#print Dumper($head);
 
 my %references;
 
-sub findCircularReferences() {
-	my $head= shift;
-	bless $head, "LinkedListNode" ;	
-	if (exists $references{$head}){
-		print  "circular reference found, node is referred to twice: ", $head, "\n";
-		print Dumper (keys %references);	
-		exit 0;
+sub findCircularReferences($);
 
-	}
+findCircularReferences($head);
 
-	else{
+sub findCircularReferences($) {
+    my $head = shift;
+    bless $head, "LinkedListNode";
+    if ( exists $references{$head} ) {
+        print "circular reference found, node is referred to twice: ", $head,
+          "\n";
+        print Dumper ( keys %references );
+        exit 0;
 
-		my $successor = $head->getSuccessor();
-		my $reftype = ref $successor;
-		if ($reftype  ne 'LinkedListNode'  )
+    }
 
-		{
+    else {
 
-			print "not a circular list, reached the end of the list\n";
-			print Dumper (keys %references);	
-			exit 0;
-		}
-		else
+        my $successor = $head->getSuccessor();
+        my $reftype   = ref $successor;
+        if ( $reftype ne 'LinkedListNode' )
 
-		{
-			$references{$head} = $head->getDatum();
-			findCircularReferences( $successor);
-		}
+        {
 
+            print "not a circular list, reached the end of the list\n";
+            print Dumper ( keys %references );
+            exit 0;
+        }
+        else
 
-	}
+        {
+            $references{$head} = $head->getDatum();
+            findCircularReferences($successor);
+        }
+
+    }
 
 }
-
-
-&findCircularReferences($head);
-
 
--- LinkedList/LinkedListNode.pm
+++ LinkedList/LinkedListNode.pm
@@ -1,44 +1,43 @@
 use strict;
 use warnings;
 
-
 package LinkedListNode;
 
 our @EXPORT = qw(new);
 
-
 my $datum;
 my $successor;
 
-sub new(){
+sub new() {
 
-	my $class = shift;
-	$datum = shift;
-	$successor = shift;
-	my $self = {"datum"=>$datum, "successor" => {}}; 	
-	bless $self , $class;
-	return $self;
+    my $class = shift;
+    $datum     = shift;
+    $successor = shift;
+    my $self = { "datum" => $datum, "successor" => {} };
+    bless $self, $class;
+    return $self;
 }
 
-sub getSuccessor(){
-	my $self = shift;
-	return $self->{"successor"}; 
+sub getSuccessor() {
+    my $self = shift;
+    return $self->{"successor"};
 }
-sub setSuccessor(){
-	my $self = shift;
-	my $successor =shift;
-	$self->{"successor"} = $successor ; 
+
+sub setSuccessor() {
+    my $self      = shift;
+    my $successor = shift;
+    $self->{"successor"} = $successor;
 }
-sub getDatum(){
-	my $self = shift;
-	return $self->{"datum"}; 
+
+sub getDatum() {
+    my $self = shift;
+    return $self->{"datum"};
 }
-sub setDatum(){
-	my $self = shift;
-	my $datum =shift;
-	$self->{"datum"} = $datum ; 
+
+sub setDatum() {
+    my $self  = shift;
+    my $datum = shift;
+    $self->{"datum"} = $datum;
 }
 return 1;
-
-
 
